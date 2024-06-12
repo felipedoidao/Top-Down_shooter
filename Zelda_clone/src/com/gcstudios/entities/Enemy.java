13286a7fbabc19595e880d0c6e37ebedb0b36c96
Felipe <felipexd02@gmail.com>
2024-06-12 13:27:50 -0300

    inimigos colidindo com inimigos

--- Zelda_clone/src/com/gcstudios/entities/Enemy.java
+++ Zelda_clone/src/com/gcstudios/entities/Enemy.java
@@ -1,5 +1,6 @@
 package com.gcstudios.entities;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import com.gcstudios.main.Game;
@@ -13,18 +14,35 @@ public class Enemy extends Entity{
     }
 
     public void tick(){
-        if((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY())){
+        if((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY()) && !isColiding((int)(x+speed), this.getY())){
             x+=speed;
         }
-         else if((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY())){
+         else if((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY()) && !isColiding((int)(x-speed), this.getY())){
             x-=speed;
          }
-        if((int)y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed))){
+        if((int)y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed)) && !isColiding(this.getX(), (int)(y+speed))){
             y+=speed;
         }
-         else if((int)y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed))){
+         else if((int)y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed)) && !isColiding(this.getX(), (int)(y-speed))){
             y-=speed;
          }
+    }
+
+    public boolean isColiding(int xnext, int ynext){
+        Rectangle enemyCurrent = new Rectangle(xnext, ynext, World.TILE_SIZE, World.TILE_SIZE);
+        for(int i = 0; i < Game.enemies.size(); i++){
+            Enemy e = Game.enemies.get(i);
+            if(e == this)
+                continue;
+
+            Rectangle targetEnemy = new Rectangle(e.getX(), e.getY(), World.TILE_SIZE, World.TILE_SIZE);
+            if(enemyCurrent.intersects(targetEnemy)){
+                return true;
+            }
+        }
+
+
+        return false;
     }
     
 }
